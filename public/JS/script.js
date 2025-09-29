487983b779a875c554d6091e2a3d0930a9377bb1
Van Thanggit config --global user.email nguyenvanthangktpm@gmail.comgit config --global user.name Van <nguyenvanthangktpm@gmail.com>
2025-09-29 21:08:49 +0700

    nested navigation

--- public/JS/script.js
+++ public/JS/script.js
@@ -5,14 +5,26 @@ const mobileMenuNav = document.getElementById("mobile-menu-nav");
 const mobileNavClose = document.getElementById("mobile-nav-close");
 menuBtnMobile.addEventListener("click", () => {
   if (isOpen) {
-    mobileMenuNav.style.display = "none";
+//     mobileMenuNav.style.display = "none";
+    mobileMenuNav.classList.remove("mobile-menu-nav-hidden");
     isOpen = false;
   } else {
-    mobileMenuNav.style.display = "block";
+//     mobileMenuNav.style.display = "block";
+    mobileMenuNav.classList.add("mobile-menu-nav-hidden");
     isOpen = true;
   }
 });
 mobileNavClose.addEventListener("click", () => {
-       mobileMenuNav.style.display = "none";
+       // mobileMenuNav.style.display = "none";
+       mobileMenuNav.classList.remove("mobile-menu-nav-hidden");
        isOpen = false;
 });
+// Tutorial-btn
+const nestedNavId = document.getElementById("nested-navigation-container-id");
+const tuTorialBtn = document.getElementById("tutorial-btn");
+
+tuTorialBtn.addEventListener("click",() =>{
+       tuTorialBtn.classList.toggle("bg-black");
+       tuTorialBtn.classList.toggle("text-white");
+       nestedNavId.classList.toggle("nested-navigation-hidden");
+})
